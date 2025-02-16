b7deed5a34a97aaaa059f7dedc25da1f106faafc
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-02-16 16:50:40 -0500

    make wezterm bold really, really obvious

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+
+return {
+	window_decorations = "RESIZE",
+	color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+	font_rules = {
+		{
+			intensity = "Bold",
+			italic = false,
+			font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+		},
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font("JetBrains Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+		},
+	},
+}
