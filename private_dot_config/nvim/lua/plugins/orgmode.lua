d410a2f3ee1bd25c8e277d488b60d9c6e02f80d5
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-04-03 16:25:12 -0400

    update nvim

--- private_dot_config/nvim/lua/plugins/orgmode.lua
+++ private_dot_config/nvim/lua/plugins/orgmode.lua
@@ -1,5 +1,12 @@
 local colors = require("tokyonight.colors").setup() -- pass in any of the config options as explained above
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "@org.agenda.scheduled", { fg = "#AAFFAA" })
+  end,
+})
+
 return {
   "nvim-orgmode/orgmode",
   event = "VeryLazy",
@@ -8,7 +15,29 @@ return {
     -- Setup orgmode
     require("orgmode").setup({
       org_agenda_files = "~/Nextcloud/obsidian-default/1lists/**",
-      org_default_notes_file = "~/Nextcloud/org/refile.org",
+      org_agenda_custom_commands = {
+        T = {
+          name = "TODO",
+          description = "match only TODO",
+          types = {
+            {
+              type = "tags_todo",
+              match = 'TODO="TODO"',
+            },
+          },
+        },
+        s = {
+          name = "Shallow",
+          description = "Shallow",
+          types = {
+            {
+              type = "tags",
+              match = "shallow", --Same as providing a "Match:" for tags view <leader>oa + m, See: https://orgmode.org/manual/Matching-tags-and-properties.html
+              org_agenda_todo_ignore_scheduled = "all", -- Ignore all headlines that are scheduled. Possible values: past | future | all
+            },
+          },
+        },
+      },
       org_todo_keywords = {
         "TODO(t)",
         "WAITING",
@@ -19,6 +48,7 @@ return {
         "|",
         "DONE",
       },
+      calendar_week_start_day = 0,
       org_todo_keyword_faces = {
         TODO = ":foreground orange", -- overrides builtin color for `TODO` keyword
         WAITING = ":slant italic",
